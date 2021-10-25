66ffa00d4d89b83d1416811a19a702bd3bd935be
gitlab-runner <do-not-reply@cypress.com>
2021-10-25 11:30:00 -0500

    Upload mtb-example-psoc6-spi-master 2.2.0.160

--- resource_map.h
+++ resource_map.h
@@ -57,7 +57,8 @@
     defined (TARGET_CY8CKIT_062_WIFI_BT) || defined (TARGET_CY8CKIT_062S2_43012) || \
     defined (TARGET_CYW9P62S1_43012EVB_01) || defined (TARGET_CY8CKIT_064B0S2_4343W) || \
     defined (TARGET_CYSBSYSKIT_01) || defined (TARGET_CYSBSYSKIT_DEV_01) || \
-    defined (TARGET_CY8CKIT_062S4)
+    defined (TARGET_CY8CKIT_062S4) || defined (TARGET_CY8CEVAL_062S2) || \
+    defined (TARGET_CY8CEVAL_062S2_LAI_4373M2)
     #define KIT_SPI_MASTER_MOSI               CYBSP_SPI_MOSI
     #define KIT_SPI_MASTER_MISO               CYBSP_SPI_MISO
     #define KIT_SPI_MASTER_SCLK               CYBSP_SPI_CLK
@@ -88,7 +89,8 @@
     #define KIT_SPI_SLAVE_SS                 CYBSP_SPI_CS
 #elif defined (TARGET_CY8CKIT_062_BLE) || defined (TARGET_CY8CKIT_062_WIFI_BT) || \
     defined (TARGET_CY8CPROTO_063_BLE) || defined (TARGET_CY8CKIT_062S2_43012) || \
-    defined (TARGET_CYW9P62S1_43438EVB_01) || defined (TARGET_CY8CKIT_064B0S2_4343W)
+    defined (TARGET_CYW9P62S1_43438EVB_01) || defined (TARGET_CY8CKIT_064B0S2_4343W) || \
+    defined (TARGET_CY8CEVAL_062S2) || defined (TARGET_CY8CEVAL_062S2_LAI_4373M2)
     #define KIT_SPI_SLAVE_MOSI               (P10_0)
     #define KIT_SPI_SLAVE_MISO               (P10_1)
     #define KIT_SPI_SLAVE_SCLK               (P10_2)
